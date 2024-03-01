8456449d8a1a2fb1db9e26c344c65572a1eb9fa1
Reazy96 <resul.sadriu@outlook.com>
2024-03-01 14:22:21 +0100

    more funktions

--- project-yugioh/src/components/Counter/Counter.jsx
+++ project-yugioh/src/components/Counter/Counter.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSpring, animated } from "react-spring";
 import "./Counter.css";
 
 const Counter = () => {
@@ -6,22 +7,49 @@ const Counter = () => {
   const [attack, setAttack] = useState("");
   let [loose, setLoose] = useState("You Loose");
 
+  const { number } = useSpring({
+    number: fullPoints,
+    from: { number: 8000 },
+    config: { duration: 3500 },
+  });
+
+  const playAudio = () => {
+    const audioElement = new Audio("../../../public/Yu-Gi-Oh Life Points.mp3");
+    audioElement.play();
+  };
+
+  const handleAttack = (value) => {
+    setFullPoints(fullPoints - Number(value));
+    playAudio();
+  };
+
+  const handleDefend = (value) => {
+    setFullPoints(fullPoints + Number(value));
+    playAudio();
+  };
+
   return (
     <section>
-      <article>
-        <h2>{fullPoints}</h2>
-      </article>
+      <fieldset>
+        <legend>Your Points</legend>
+        <h2>
+          <animated.span>{number.to((n) => n.toFixed(0))}</animated.span>
+        </h2>
+      </fieldset>
+
       <input
         type="Number"
-        placeholder="Punkte"
+        placeholder=""
         onChange={(event) => setAttack(Number(event.target.value))}
         value={attack}
       />
       <div>
-        <h3 onClick={() => setFullPoints(fullPoints - Number(attack))}>-</h3>
-        <h3 onClick={() => setFullPoints(fullPoints + Number(attack))}>+</h3>
+        <h3 onClick={() => handleAttack(attack)}>-</h3>
+        <h3 onClick={() => handleDefend(attack)}>+</h3>
       </div>
       {fullPoints <= 0 ? <h6> You Loose </h6> : ""}
+
+      <audio controls src="../../../public/Yu-Gi-Oh Life Points.mp3"></audio>
     </section>
   );
 };
